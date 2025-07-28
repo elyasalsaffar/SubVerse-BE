eb88dba117e6c0715603e615a8e22ff8bde3c708
elyasalsaffar <elyas.dusty@gmail.com>
2025-07-28 18:44:22 +0300

    Initial building of backend

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const AuthRouter = require('./routes/AuthRouter')
+
+const PORT = process.env.PORT || 3001
+
+const db = require('./db')
+
+const app = express()
+
+// app.use(cors({
+    // Include the frontend deployment link
+    // origin: '',
+    // credentials: true
+// }))
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/auth', AuthRouter)
+
+app.use('/', (req, res) => {
+    res.send('Connected!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Running Express server on Port ${PORT} . . .`)
+})
