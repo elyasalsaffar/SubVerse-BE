4394438b32db8ad1a10f2b21063f8981469328c3
elyasalsaffar <elyas.dusty@gmail.com>
2025-07-30 20:34:16 +0300

    Models index.js updated

--- models/index.js
+++ models/index.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose')
 const userSchema = require('./User')
+const postSchema = require('./Post')
+const commentSchema = require('./Comment')
+const reportSchema = require('./Report')
+const subverseSchema = require('./Subverse')
+const voteSchema = require('/Vote')
 
 const User = mongoose.model('User', userSchema)
+const Post = mongoose.model('Post', postSchema)
+const Comment = mongoose.model('Comment', commentSchema)
+const Report = mongoose.model('Report', reportSchema)
+const Subverse = mongoose.model('Subverse', subverseSchema)
+const Vote = mongoose.model('Vote', voteSchema)
 
 module.exports = {
-    User
+    User,
+    Post,
+    Comment,
+    Report,
+    Subverse,
+    Vote
 }
